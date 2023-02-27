bcc2e1ae978f29b224f866e7dc30198b0d19df6e
JeffreyDeras <jefreyjb10@gmail.com>
2023-02-27 16:28:46 -0300

    second commit

---
+++ desafio-dio-POO/src/Calculadora.java
@@ -0,0 +1,10 @@
+public class Calculadora {
+    public static void main(String[] args) {
+
+        int numero1 = 10;
+        int numero2 = 5;
+        int soma = numero1 + numero2;
+
+        System.out.println(soma);
+    }
+}
