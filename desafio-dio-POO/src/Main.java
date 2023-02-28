8d7417e0a2822935e0341981960c946d319ea6c6
JeffreyDeras <jefreyjb10@gmail.com>
2023-02-28 13:16:20 -0300

    Third commit

--- desafio-dio-POO/src/Main.java
+++ desafio-dio-POO/src/Main.java
@@ -1,5 +1,29 @@
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Curso curso1 = new Curso();
+        curso1.setTitulo("Curso Java");
+        curso1.setDescricao("Descrição Curso Java");
+        curso1.setCargaHoraria(80);
+
+        Curso curso2 = new Curso();
+        curso2.setTitulo("Curso Js");
+        curso2.setDescricao("Descrição Curso Js");
+        curso2.setCargaHoraria(75);
+
+
+        Mentoria mentoria1 = new Mentoria();
+        mentoria1.setTitulo("Mentoria Programação");
+        mentoria1.setDescricao("Descrição Mentoria");
+        mentoria1.setData(LocalDate.now());
+
+        System.out.println(curso1);
+        System.out.println(curso2);
+        System.out.println(mentoria1);
     }
 }
